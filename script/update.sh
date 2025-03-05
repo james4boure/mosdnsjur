3c38cc4618876ece1aefd7e8e5287f7476184571
james4boure <117244171+james4boure@users.noreply.github.com>
2025-03-05 14:42:33 +0800

    Update update.sh
    copy from jouranlist

--- script/update.sh
+++ script/update.sh
@@ -4,26 +4,26 @@
 #
 mosdns_working_dir="/etc/mosdns"
 mkdir -p /tmp/easymosdns \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/akamai_domain_list.txt > /tmp/easymosdns/akamai_domain_list.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/block_list.txt > /tmp/easymosdns/block_list.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/cachefly_ipv4.txt > /tmp/easymosdns/cachefly_ipv4.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/cdn77_ipv4.txt > /tmp/easymosdns/cdn77_ipv4.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/china_domain_list_mini.txt > /tmp/easymosdns/china_domain_list_mini.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/cloudfront.txt > /tmp/easymosdns/cloudfront.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/cloudfront_ipv6.txt > /tmp/easymosdns/cloudfront_ipv6.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/custom_list.txt > /tmp/easymosdns/custom_list.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/gfw_ip_list.txt > /tmp/easymosdns/gfw_ip_list.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/grey_list.txt > /tmp/easymosdns/grey_list.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/hosts_akamai.txt > /tmp/easymosdns/hosts_akamai.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/hosts_fastly.txt > /tmp/easymosdns/hosts_fastly.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/jp_dns_list.txt > /tmp/easymosdns/jp_dns_list.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/original_domain_list.txt > /tmp/easymosdns/original_domain_list.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/ipv6_domain_list.txt > /tmp/easymosdns/ipv6_domain_list.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/private.txt > /tmp/easymosdns/private.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/redirect.txt > /tmp/easymosdns/redirect.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/sucuri_ipv4.txt > /tmp/easymosdns/sucuri_ipv4.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/us_dns_list.txt > /tmp/easymosdns/us_dns_list.txt \
-&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/c3b0391b9c6cff72171146b8be259c30866b8989/white_list.txt > /tmp/easymosdns/white_list.txt
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/akamai_domain_list.txt > /tmp/easymosdns/akamai_domain_list.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/block_list.txt > /tmp/easymosdns/block_list.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/cachefly_ipv4.txt > /tmp/easymosdns/cachefly_ipv4.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/cdn77_ipv4.txt > /tmp/easymosdns/cdn77_ipv4.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/china_domain_list_mini.txt > /tmp/easymosdns/china_domain_list_mini.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/cloudfront.txt > /tmp/easymosdns/cloudfront.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/cloudfront_ipv6.txt > /tmp/easymosdns/cloudfront_ipv6.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/custom_list.txt > /tmp/easymosdns/custom_list.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/gfw_ip_list.txt > /tmp/easymosdns/gfw_ip_list.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/grey_list.txt > /tmp/easymosdns/grey_list.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/hosts_akamai.txt > /tmp/easymosdns/hosts_akamai.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/hosts_fastly.txt > /tmp/easymosdns/hosts_fastly.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/jp_dns_list.txt > /tmp/easymosdns/jp_dns_list.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/original_domain_list.txt > /tmp/easymosdns/original_domain_list.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/ipv6_domain_list.txt > /tmp/easymosdns/ipv6_domain_list.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/private.txt > /tmp/easymosdns/private.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/redirect.txt > /tmp/easymosdns/redirect.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/sucuri_ipv4.txt > /tmp/easymosdns/sucuri_ipv4.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/us_dns_list.txt > /tmp/easymosdns/us_dns_list.txt \
+&& curl https://ghproxy.cc/https://raw.githubusercontent.com/Journalist-HK/Rules/main/white_list.txt > /tmp/easymosdns/white_list.txt \
 && curl https://ghproxy.cc/https://raw.githubusercontent.com/Loyalsoldier/geoip/release/text/facebook.txt > /tmp/easymosdns/facebook.txt \
 && curl https://ghproxy.cc/https://raw.githubusercontent.com/Loyalsoldier/geoip/release/text/fastly.txt > /tmp/easymosdns/fastly.txt \
 && curl https://ghproxy.cc/https://raw.githubusercontent.com/Loyalsoldier/geoip/release/text/telegram.txt > /tmp/easymosdns/telegram.txt \
